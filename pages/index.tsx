7aaa19f345d8f6687828c6d95f7c692f12a06945
Rostyslav Nazymko <rostiknaz@gmail.com>
2023-01-01 16:16:56 +0000

    feat: create CatCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { Inter } from '@next/font/google';
 import Head from 'next/head';
 import Image from 'next/image';
+import CatCard from '../components/cards/cat/CatCard';
+import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
 import styles from '../styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -39,24 +41,11 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <div className={styles.grid}>
+          <CatCard {...mockCatCardProps.base} />
+          <CatCard {...mockCatCardProps.base} />
+          <CatCard {...mockCatCardProps.base} />
+          <CatCard {...mockCatCardProps.base} />
         </div>
 
         <div className={styles.grid}>
